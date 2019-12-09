c1cfb45d36aab612fdfe054fab9386f006bda036
Vishweswar53 <vishweswar53@gmail.com>
2019-12-09 20:51:41 +0530

    Done making angular login and signup page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { AppComponent } from './app.component';
-import { PoseEstimationComponent } from './pose-estimation/pose-estimation.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignUpComponent } from './auth/signup/signup.component';
+import { PoseEstimationComponent } from './pose-estimation/pose-estimation.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'auth/signup', component: SignUpComponent },
+  { path: 'pose', component: PoseEstimationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignUpComponent implements OnInit {
+  isActive = true;
+  constructor() { }
+
+  ngOnInit(): void { }
+}
