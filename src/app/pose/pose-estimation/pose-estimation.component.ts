3cdf6bef9d3beefaf8d83942fe0f6d9b6faa1e42
Vishweswar53 <vishweswar53@gmail.com>
2019-12-11 16:43:31 +0530

    Paused the running video

--- src/app/pose/pose-estimation/pose-estimation.component.ts
+++ src/app/pose/pose-estimation/pose-estimation.component.ts
@@ -131,6 +131,9 @@ export class PoseEstimationComponent implements OnInit, OnDestroy {
           this.video.play();
         };
       });
+    if (this.time === 0) {
+      this.video.pause();
+    }
   }
 
   renderPredictions = (prediction: posenet.Pose) => {
