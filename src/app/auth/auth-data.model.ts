0764afede16a5bf4564f3bee99f452b6bee6ab67
Vishweswar53 <vishweswar53@gmail.com>
2019-12-14 19:56:06 +0530

    Saved the updated version

--- src/app/auth/auth-data.model.ts
+++
@@ -1,5 +0,0 @@
-export interface AuthData {
-  fullname: string;
-  email: string;
-  password: string;
-}
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { LoadingBarService } from '@ngx-loading-bar/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent implements OnInit, OnDestroy {
   isActive = true;
+  private authStatusSub: Subscription;
+  constructor(private authService: AuthService, public loader: LoadingBarService) { }
 
-  constructor(public authService: AuthService, public loader: LoadingBarService) { }
-
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.authStatusSub = this.authService.getAuthStatusListener().subscribe(
+      (authStatus) => {
+        console.log(authStatus);
+      }
+    );
+  }
 
   onSignup(form: NgForm) {
     if (form.invalid) {
@@ -22,5 +29,9 @@ export class SignUpComponent implements OnInit {
     this.loader.start();
     this.authService.createUser(form.value.fullname, form.value.email, form.value.password);
     this.loader.complete();
+  }
+
+  ngOnDestroy(): void {
+    this.authStatusSub.unsubscribe();
   }
 }
